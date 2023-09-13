777845dcef73257891a83062c819967594f13262
MaksKuramshev <96337256+MaksKuramshev@users.noreply.github.com>
2023-09-13 14:32:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,110 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHeight: true,
+        //autoplay: true,
+        //autoplaySpeed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+       
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //modals
+
+    $('[data-modal=consultatin]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultatin, #thanks, #order').fadeOut('fast');
+    });
+    //show order modal
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    //validate    
+    function valideForms(form){
+        $(form).validate({
+            rules : {
+                name: {
+                    required:true,
+                    minlength:2
+                },
+                phone: "required",
+                email:  {
+                    required:true,
+                    email:true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Введите ваше имя",
+                    minlength: jQuery.validator.format("Введите минимум {0} символа!")
+                  },
+                phone: "Введите ваш номер",
+                email: {
+                  required: "Введите почту",
+                  email: "Введите адрес в формате : name@domain.com"
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation form');
+    valideForms('#consultation-form');
+    valideForms('#order form');
+
+    //mask
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //send mail
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+//
+        if (!$(this).valid()) {
+            return;
+        }
+//
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+
+            $('#consultation , #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+});
